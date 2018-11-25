f4a287c86ce8b0e4f50106e013f1c0b8d7ca8763
StacyChebet <staceychebet23@gmail.com>
2018-11-25 11:16:24 +0300

    initialize db app instance

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -9,4 +9,7 @@ def create_app(config_name):
 
     #Creating the app configurations
     app.config.from_object(config_options[config_name])
-    
+
+    #Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
