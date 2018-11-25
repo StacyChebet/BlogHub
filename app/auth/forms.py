45d84c568e6201aa241291a27d68c01bef3af590
StacyChebet <staceychebet23@gmail.com>
2018-11-25 19:53:52 +0300

    Add function to validate email

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -12,3 +12,7 @@ class SignupForm(FlaskForm):
     password_confirm = PasswordField(
         'Confirm Password', validators=[Required()])
     submit = SubmitField('Sign Up')
+
+    def validate_email(self, data_field):
+        if User.query.filter_by(email=data_field.data).first():
+            raise ValidationError("Account already exists")
