d18c0fbabf381ed3a8cbd58674a19fe163d32f8a
StacyChebet <staceychebet23@gmail.com>
2018-11-25 20:06:02 +0300

    add the function that returns the login page

--- app/auth/views.py
+++ app/auth/views.py
@@ -21,3 +21,18 @@ def signup():
         return redirect(url_for('auth.login'))
         title = "Create New Account"
     return render_template('auth/signup.html', signup_form=form)
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email=login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash("Invalid username or Password")
+
+    title = "Speed Pitching Login"
+    return render_template('auth/login.html', login_form=login_form, title=title)
