256162d1d2afc659b9bf28030109d76f7d6a627f
StacyChebet <staceychebet23@gmail.com>
2018-11-25 10:58:27 +0300

    Create the app configurations

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -7,4 +7,6 @@ bootstrap = Bootstrap()
 def create_app(config_name):
     app = Flask(__name__)
 
+    #Creating the app configurations
+    app.config.from_object(config_options[config_name])
     
