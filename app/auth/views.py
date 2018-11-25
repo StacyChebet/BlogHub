50cfc77440fe4dfd11ab8320e7ce2ede585bb426
StacyChebet <staceychebet23@gmail.com>
2018-11-25 21:07:39 +0300

    add view function for logging out that returns the index page

--- app/auth/views.py
+++ app/auth/views.py
@@ -34,5 +34,12 @@ def login():
 
         flash("Invalid username or Password")
 
-    title = "Speed Pitching Login"
+    title = "BlogHub Login"
     return render_template('auth/login.html', login_form=login_form, title=title)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
