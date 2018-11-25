fb216504603c789771ac70c8e77970555ddb5f83
StacyChebet <staceychebet23@gmail.com>
2018-11-25 21:22:16 +0300

    update 'users' table

--- app/models.py
+++ app/models.py
@@ -1,9 +1,22 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True, index=True)
+    profile = db.Column(db.String)
+    prof_pic = db.Column(db.String())
+    pass_hash = db.column(db.String(255))
 
+    
     def __repr__(self):
         return f'User {self.username}'
