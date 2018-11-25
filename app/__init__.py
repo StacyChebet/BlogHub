dd3cae4a0b02790c517c0960f584c037dafb9cbe
StacyChebet <staceychebet23@gmail.com>
2018-11-25 17:08:56 +0300

    Register the blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -14,4 +14,8 @@ def create_app(config_name):
     bootstrap.init_app(app)
     db.init_app(app)
 
+    #Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
     return app
