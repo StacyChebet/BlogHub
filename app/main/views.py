aad47be8356b7ee2e904fc683a7e2e616ec573fa
StacyChebet <staceychebet23@gmail.com>
2018-11-26 13:58:28 +0300

    Add view function for profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,23 @@
 from flask import render_template,request,redirect,url_for
 from . import main 
-from .forms import 
-from ..models import User
+# from .forms import
+from ..models import User,Blog
 
 #Views
 @main.route('/')
 def index():
     '''
-    Renders index page
+    Retruns index page
     '''
-    title = "Home"
+    title = "BLOG HUB"
     return render_template('index.html', title = title)
+
+
+@main.route('/user/<username>')
+def profile(username):
+    user = User.query.filter_by(username=username).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user=user)
