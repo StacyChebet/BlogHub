61ea2b33c25b65c49829a47e18eeab759916353c
StacyChebet <staceychebet23@gmail.com>
2018-11-26 14:30:22 +0300

    add view function for updating profile picture

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,10 @@
 from flask import render_template,request,redirect,url_for
-from . import main 
-# from .forms import
 from ..models import User,Blog
+from . import main 
+from flask_login import login_required
+from .forms import UpdateProfile
+from .. import db,photos
+import datetime
 
 #Views
 @main.route('/')
@@ -21,3 +24,35 @@ def profile(username):
         abort(404)
 
     return render_template("profile/profile.html", user=user)
+
+
+@main.route('/user/<username>/update', methods=['GET', 'POST'])
+@login_required
+def update_profile(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.profile = form.profile.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile', username=user.username))
+
+    return render_template('profile/update.html', form=form)
+
+
+@main.route('/user/<username>/update/pic', methods=['POST'])
+@login_required
+def update_pic(username):
+    user = User.query.filter_by(username=username).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.prof_pic = path
+        db.session.commit()
+    return redirect(url_for('main.profile', username=username))
