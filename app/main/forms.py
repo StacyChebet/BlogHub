fa3e33d74ecea6a37757bdcb5932895a3eb7cdd1
StacyChebet <staceychebet23@gmail.com>
2018-11-27 06:25:27 +0300

    Add the form to be used to add blogs by admin

---
+++ app/main/forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField, SelectField, FileField
+from wtforms.validators import Required,Email
+
+class UpdateProfile(FlaskForm):
+    profile = TextAreaField("Tell us about you", validators=[Required()])
+    submit = SubmitField("Add")
+
+class AddBlog(FlaskForm):
+    title = StringField('Title', validators=[Required()])
+    category = SelectField('Category', choices=[('politics', 'Politics'), ('fashion', 'Fashion'), (
+        'travel', 'Travel')], validators=[Required()])
+    photo = FileField('Select an image', validators=[Required()])
+    content = TextAreaField('Blog', validators=[Required()])
+    submit = SubmitField('Post')
+
+
+
+    
+
+    
